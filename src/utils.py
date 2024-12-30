1a5052f47e504ebba9b9d49c8b5ac3b30f96f738
Ojas <ozsmadghe@gmail.com>
2024-12-30 15:40:13 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,17 @@ import pandas as pd
 from src.logger import logging
 from src.exception import CustomException
 import dill
+from dataclasses import dataclass
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from src.exception import CustomException
+from src.logger import logging
+
 
 
 def save_object(file_path, obj):
@@ -14,5 +25,20 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+def evaluate_model(X_train, Y_train,X_test,Y_test,models:dict):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, Y_train)
+            y_pred = model.predict(X_test)
+            r2 = r2_score(Y_test, y_pred)
+            report[list(models.keys())[i]]=r2
+        
+        return report
     except Exception as e:
         raise CustomException(e,sys)
